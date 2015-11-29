5690586469ec1043ee0eb84870c90e4f54c13d58
patron <patron@Mac-6476baa4e424.fios-router.home>
2015-11-29 14:44:04 -0600

    P5, donish

--- P5/handin/P5.java
+++ P5/handin/P5.java
@@ -20,6 +20,7 @@ public class P5 {
 	public static final int RESULT_CORRECT = 0;
 	public static final int RESULT_SYNTAX_ERROR = 1;
 	public static final int RESULT_TYPE_ERROR = 2;
+	public static final int RESULT_NAME_ERROR = 3;
 	public static final int RESULT_OTHER_ERROR = -1;
 
 	/**
@@ -145,6 +146,11 @@ public class P5 {
 		
         ProgramNode astRoot = (ProgramNode)cfgRoot.value; 
 		astRoot.nameAnalysis();  // perform name analysis
+		
+		if (ErrMsg.getErr()) {  
+			return P5.RESULT_NAME_ERROR;
+		}
+		
 		astRoot.unparse(outFile, 0);
 		
 		if (!astRoot.typeCheck()){
@@ -152,7 +158,6 @@ public class P5 {
 			return P5.RESULT_TYPE_ERROR;
 		}
 		
-		astRoot.unparse(outFile, 0);
 		return P5.RESULT_CORRECT;
 	}
 	
@@ -166,6 +171,8 @@ public class P5 {
 		switch(resultCode){
 		case RESULT_SYNTAX_ERROR:
 			pukeAndDie("Syntax error", resultCode);
+		case RESULT_NAME_ERROR:
+			pukeAndDie("Name analysis error", resultCode);
 		case RESULT_TYPE_ERROR:
 			pukeAndDie("Type checking error", resultCode);
 		default:
